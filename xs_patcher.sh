dc92c0fe78436d774fad9e2bc9c64446127ad9ac
Scott Noel-Hemming <frogstarr78@gmail.com>
2016-09-10 20:40:24 -0700

    Removing Patch files as we go. Plus adding some exception handling

--- xs_patcher.sh
+++ xs_patcher.sh
@@ -8,6 +8,10 @@ source /etc/xensource-inventory
 TMP_DIR=$HOME/tmp
 CACHE_DIR=$HOME/cache
 
+function clean_tmp_dir {
+	rm -rf $TMP_DIR/*
+}
+
 function get_xs_version {
 	get_version=`cat /etc/redhat-release | awk -F'-' {'print $1'}`
 	case "${get_version}" in
@@ -57,16 +61,37 @@ function apply_patches {
 				echo "Downloading from $PATCH_URL..."
 				wget -q $PATCH_URL -O $TMP_DIR/$PATCH_FILE
 				echo "...unpaching"
-				unzip -qq $TMP_DIR/$PATCH_FILE -d $CACHE_DIR && rm $TMP_DIR/$PATCH_FILE
+				unzip -qq $TMP_DIR/$PATCH_FILE -d $CACHE_DIR 
+				rm $TMP_DIR/$PATCH_FILE
 			fi	
 
 			echo "Applying $PATCH_NAME... [ Release Notes @ $PATCH_KB ]"
-			xe patch-upload file-name=$CACHE_DIR/$PATCH_NAME.xsupdate
-			xe patch-apply uuid=$PATCH_UUID host-uuid=$INSTALLATION_UUID
+			UP_PATCH_UUID=$(xe patch-upload file-name=$CACHE_DIR/$PATCH_NAME.xsupdate)
+			if [[ -z $UP_PATCH_UUID ]]; then
+				echo "Patch $PATCH_UUID failed to upload. Check that the patch file exists ($CACHE_DIR/$PATCH_NAME.xsupdate) and try again."
+			else
+				if [[ $UP_PATCH_UUID == $PATCH_UUID ]]; then
+					xe patch-apply uuid=$PATCH_UUID host-uuid=$INSTALLATION_UUID
+					if [[ $? -eq 0 ]]; then
+						rm $CACHE_DIR/${PATCH_NAME}.xsupdate
+						rm $CACHE_DIR/${PATCH_NAME}*.bz2
+						if [[ $DISTRO == "creedence" && $PATCH_NAME == "XS65ESP1023" ]]; then
+							clean_tmp_dir
+							echo "For some reason XS65ESP1024 errors when here. Reboot the server and clean up the old patches. Then rerun me"
+							exit 1
+						fi
+					else
+						break
+						echo "The patch $PATCH_NAME failed to apply to the host. Check to make sure the disk isn't full and re-run the script."
+					fi
+				else
+					echo "Patch ID \"$PATCH_UUID\" doesn't match the patch ID \"$UP_PATCH_UUID\" returned from the uploaded patch. Try re-running the script if this appears to be an invalid error. Otherwise try applying the patch using the returned ID, again \"$UP_PATCH_UUID\"."
+				fi
+			fi
 		fi
 	done
 
-	rm -rf $TMP_DIR/*
+	clean_tmp_dir
 	echo "Everything has been patched up!"
 }
 
